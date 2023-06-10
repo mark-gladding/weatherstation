7b586b7c1d168e947746dbc88e6995e2d911d78a
zmg <mark.gladding@invetech.com.au>
2023-06-10 22:02:41 +1000

    Updated the settings.py to make loading from file more generic.

--- settings.py
+++ settings.py
@@ -1,25 +1,37 @@
+# Module responsible for providing configurable settings for this application.
+#
+# Copyright (C) Mark Gladding 2023.
+#
+# MIT License (see the accompanying license file)
+#
+# https://github.com/mark-gladding/weatherstation
+#
+
 import json
 import os
 import sys
 
-aws_region = 'ap-southeast-2'
-ntp_time_server = 'au.pool.ntp.org'
+sensor_read_period_s = 60     # Read the sensors every 60 seconds
 
+# Time related settings. Update for your geographic location.
+ntp_time_server = 'au.pool.ntp.org'
+sync_time_period = 30 * 60 / sensor_read_period_s   # Sync the RTC with the ntp time server every 30 minutes
+
+timezone_location = '-37.9707183%2C144.392352'  # Australia / Melbourne
+
+# AWS Timestream Settings
+aws_region = 'ap-southeast-2'
 database_name = 'WeatherDb'
 sensor_readings_table = 'Weather'
 device_log_table = 'DeviceLog'
 
-sensor_read_period_s = 60     # Read the sensors every 60 seconds
 
 display_cycle_period_ms = 5000  # Time to display a reading on the display before moving to the next reading (e.g remote sensor temperature).
 
-sync_time_period = 30 * 60 / sensor_read_period_s   # Sync the RTC with the ntp time server every 30 minutes
-
 reboot_on_error = True      # If true, reboot the Pico on error. If false, just raise the exception (useful during development)
 
-timezone_location = '-37.9707183%2C144.392352'  # Australia / Melbourne
 
-# Load the location specific settings from a 'settings_[location].json' file.
+# Load the sensor-specific settings from a 'settings_[location].json' file.
 files = os.listdir()
 settings_file =  [file for file in files if file.startswith('settings_')]
 if len(settings_file) == 0:
@@ -29,10 +41,8 @@ settings_file = settings_file[0]
 try:
     with open(settings_file) as f:
         location_settings = json.load(f)
-        sensor_location = location_settings['sensor_location']
-        remote_sensor_location = location_settings['remote_sensor_location']
-        draw_power_period_s = location_settings['draw_power_period_s']
-        deep_sleep = location_settings['deep_sleep']
+        # Merge the settings loaded from the json file into this module.
+        sys.modules[__name__].__dict__.update(location_settings)
 
 except OSError:
     sys.exit(f'Cannot load location specific settings file {settings_file}.')
